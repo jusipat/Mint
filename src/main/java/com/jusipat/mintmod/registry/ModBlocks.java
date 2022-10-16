ec222c5ff3fbdc585d449c935192e1d85c3eb71a
jusipat <justin10patrick@gmail.com>
2022-10-16 15:22:57 -0600

    Added iron coin Began mint implementation

--- src/main/java/com/jusipat/mintmod/registry/ModBlocks.java
+++ src/main/java/com/jusipat/mintmod/registry/ModBlocks.java
@@ -1,8 +1,17 @@
 package com.jusipat.mintmod.registry;
 
+import com.jusipat.mintmod.MintMod;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class ModBlocks {
 
-    public static void registerBlocks() {
+    public static final Block MINT = new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f));
 
+    public static void registerBlocks() {
+        Registry.register(Registry.BLOCK, new Identifier(MintMod.MOD_ID, "mint"), MINT);
     }
 }
--- src/main/java/com/jusipat/mintmod/registry/ModItems.java
+++ src/main/java/com/jusipat/mintmod/registry/ModItems.java
@@ -2,16 +2,22 @@ package com.jusipat.mintmod.registry;
 
 import com.jusipat.mintmod.MintMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item COPPER_COIN = new Item(new FabricItemSettings().group(MintMod.MINT_GROUP));
+    public static final Item IRON_COIN = new Item(new FabricItemSettings().group(MintMod.MINT_GROUP));
     public static final Item GOLD_COIN = new Item(new FabricItemSettings().group(MintMod.MINT_GROUP));
 
 public static void registerItems() {
     Registry.register(Registry.ITEM, new Identifier(MintMod.MOD_ID, "copper_coin"), COPPER_COIN);
+    Registry.register(Registry.ITEM, new Identifier(MintMod.MOD_ID, "iron_coin"), IRON_COIN);
     Registry.register(Registry.ITEM, new Identifier(MintMod.MOD_ID, "gold_coin"), GOLD_COIN);
+
+    Registry.register(Registry.ITEM, new Identifier(MintMod.MOD_ID, "mint"), new BlockItem(ModBlocks.MINT, new FabricItemSettings().group(MintMod.MINT_GROUP)));
     }
 }
